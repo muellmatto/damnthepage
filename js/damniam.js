4d32cd1d51c4e6d3b2b9918ff36ef8cac26bb546
matto <matto@damniam.de>
2016-05-31 01:20:19 +0200

    jsonp fix

--- js/damniam.js
+++ js/damniam.js
@@ -59,15 +59,16 @@ function fillGrid(data) {
 function fillGallery(data) {
     var template = $('#gallerytemplate').html();
     Mustache.parse(template);   // optional, speeds up future uses
-    for (i in data) {
-        var datum = new Date(parseInt(data.data[0].created_time) * 1000);
+    for (i in data.data) {
+        var datum = new Date(parseInt(data.data[1].created_time) * 1000);
             var rendered = Mustache.render(template, {
                                             image: data.data[i].images.low_resolution.url,
+                                            link: data.data[i].link,
                                             datum: datum.toLocaleDateString()
                                             }
                                         );
             // document.getElementById('termine').innerHTML += rendered;
-            $('#gallery').append(rendered);
+            $('#media').append(rendered);
      }
 }
 
@@ -77,6 +78,6 @@ $( document ).ready(function() {
     $.getJSON( 'http://api.bandsintown.com/artists/damniam/events.json?api_version=2.0&app_id=damniam_website&callback=?', fillTable );
     $.getJSON( 'https://graph.facebook.com/v2.6/35075947587/posts?fields=full_picture,message,link,created_time&limit=10&access_token=1280679008628028|iSLmie0AppAKj2yWz3zx2TN8C4Q', fillGrid);
     // $.getJSON( 'https://graph.facebook.com/v2.6/35075947587/posts?fields=full_picture,message,link,created_time&limit=10&access_token=EAACEdEose0cBAI5zCanZB0VXacZASI4JHCgxd2s2YcyVEPyOtPPgxoCKd10TymNXVfFswBDtV3jQp3JSJLEBuZBkugIuAvfzxarovACSPdSbH8uyrjhl4FjZAZAzHeRo6ppP2ZBZCfWh4a5CfKrhPiZAPkT9EV8H5OJ4v7R7dyJykgZDZD', fillGrid);
-    $.getJSON( 'https://api.instagram.com/v1/users/self/media/recent/?access_token=328950673.467ede5.f0004ab84606494dbde9242463601438&count=16', fillGallery );
+    $.getJSON( 'https://api.instagram.com/v1/users/self/media/recent/?access_token=328950673.467ede5.f0004ab84606494dbde9242463601438&count=16&callback=?', fillGallery );
 
 });
