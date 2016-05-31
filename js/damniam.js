9467bafa43973a24af589bccad492d7a66e20908
matto <matto@damniam.de>
2016-06-01 00:10:09 +0200

    band and impressum

--- js/damniam.js
+++ js/damniam.js
@@ -69,7 +69,7 @@ function fillGallery(data) {
     var template = $('#gallerytemplate').html();
     Mustache.parse(template);   // optional, speeds up future uses
     for (i in data.data) {
-        var datum = datumString(new Date(parseInt(data.data[1].created_time) * 1000));
+        var datum = datumString(new Date(parseInt(data.data[i].created_time) * 1000));
         var rendered = Mustache.render(template, {
                                         image: data.data[i].images.low_resolution.url,
                                         link: data.data[i].link,
@@ -85,6 +85,6 @@ $( document ).ready(function() {
 
     $.getJSON( 'http://api.bandsintown.com/artists/damniam/events.json?api_version=2.0&app_id=damniam_website&callback=?', fillTable );
     $.getJSON( 'https://graph.facebook.com/v2.6/35075947587/posts?fields=full_picture,message,link,created_time&limit=10&access_token=1280679008628028|iSLmie0AppAKj2yWz3zx2TN8C4Q', fillGrid);
-    $.getJSON( 'https://api.instagram.com/v1/users/self/media/recent/?access_token=328950673.467ede5.f0004ab84606494dbde9242463601438&count=16&callback=?', fillGallery );
+    $.getJSON( 'https://api.instagram.com/v1/users/self/media/recent/?access_token=328950673.467ede5.f0004ab84606494dbde9242463601438&count=24&callback=?', fillGallery );
 
 });
