4ac07a272c9c2f237084a60d915287b422de9c93
matto <matto@damniam.de>
2016-06-02 09:01:30 +0200

    shortened timestring from fb graph for safari

--- js/damniam.js
+++ js/damniam.js
@@ -39,11 +39,10 @@ function fillGrid(data) {
     var template = document.getElementById('feedtemplate').innerHTML;
     var k = 0;
     for (i in data.data) {
-        /* Safari needs a "T" for time ... m( */
-        var datum = datumString(new Date(Date.parse(data.data[i].created_time.replace(/\s/,'T'))));
+        /* Safari quirk ... m( */
+        var datum = datumString(new Date(Date.parse(data.data[i].created_time.substring(0,10) )));
             if( ("message" in data.data[i]) && ( k < 10) ) {
-                var rendered = template.replace('{{image}}',data.data[i].full_picture).replace('{{link}}',data.data[i].link).replace('{{datum}}',data.data[i].created_time).replace('{{message}}', data.data[i].message);
-                // var rendered = template.replace('{{image}}',data.data[i].full_picture).replace('{{link}}',data.data[i].link).replace('{{datum}}',datum).replace('{{message}}', data.data[i].message);
+                var rendered = template.replace('{{image}}',data.data[i].full_picture).replace('{{link}}',data.data[i].link).replace('{{datum}}',datum).replace('{{message}}', data.data[i].message);
                 document.getElementById('grid').innerHTML += rendered;
                 k++;
             };
